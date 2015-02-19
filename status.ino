a7a0b3509b135eeb5fc5d9887a49454f8d76d779
Pedro Belo <pedro@heroku.com>
2015-02-18 18:12:26 -0800

    expose api

--- status.ino
+++ status.ino
@@ -1,23 +1,51 @@
 #include "SparkButton.h"
 
+#define OFF 0
+#define GREEN 1
+#define RED 2
+
+
 SparkButton strip = SparkButton();
+
+int current[12];
+int desired[12];
 
 void setup() {
     strip.begin();
     strip.allLedsOff();
+    for(int i=0; i<12; i++) {
+        current[i] = OFF;
+        desired[i] = OFF;
+    }
+    Spark.function("update", handleUpdate);
 }
 
 void loop() {
     for(int i=0; i<12; i++) {
-        if (rand() % 2 == 0) {
-            strip.ledOff(i);
-        }
-        else {
-            int r = rand() % 255;
-            int g = rand() % 255;
-            int b = rand() % 255;
-            strip.ledOn(i, r, g, b);
+        if (current[i] != desired[i]) {
+            updateColor(i, desired[i]);
+            current[i] = desired[i];
         }
     }
-    delay(50);
+}
+
+void updateColor(int i, int state) {
+    switch(state) {
+        case OFF:
+            strip.ledOff(i);
+            break;
+        case GREEN:
+            strip.ledOn(i, 0, 10, 0);
+            break;
+        case RED:
+            strip.ledOn(i, 10, 0, 0);
+            break;
+    }
+}
+
+int handleUpdate(String command) {
+    int n = atoi(command.c_str());
+    desired[n] = GREEN;
+    desired[n+1] = RED;
+    return 1;
 }
